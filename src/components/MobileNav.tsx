b5835ddc36b6179baace06ff59ee5b042be499a8
UmeshCity <umesh_desai6@yahoo.com>
2024-06-15 11:04:01 +1000

    Add logout button on Mobile Nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -8,15 +8,18 @@ import {
 } from "@/components/ui/sheet"
 import { sidebarLinks } from "@/constants"
 import { cn } from "@/lib/utils"
-import { useUser } from "@clerk/nextjs"
+import { SignedIn, SignedOut, useUser, useClerk } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
+import { Button } from "./ui/button"
 
 
 const MobileNav = () => {
   const { user } = useUser();
   const pathname = usePathname();
+  const { signOut } = useClerk();
+  const router = useRouter();
 
   return (
     <section>
@@ -32,20 +35,34 @@ const MobileNav = () => {
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
               <nav className="flex h-full flex-col gap-6 text-white-1">
-              {sidebarLinks.map(({ route, label, imgURL }) => {
-               
-                const routePath = route.startsWith('/profile')? `/profile/${user?.id}` : route;
-                const isActive = pathname === routePath || pathname.startsWith(`${routePath}/`);
-                
-                return <SheetClose asChild key={routePath}><Link href={routePath} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-start", {
-                  'bg-nav-focus border-r-4 border-orange-1': isActive
-                })}>
-                  <Image src={imgURL} alt={label} width={24} height={24} />
-                  <p>{label}</p>
-                </Link></SheetClose>
-              })}
+                {sidebarLinks.map(({ route, label, imgURL }) => {
+
+                  const routePath = route.startsWith('/profile') ? `/profile/${user?.id}` : route;
+                  const isActive = pathname === routePath || pathname.startsWith(`${routePath}/`);
+
+                  return <SheetClose asChild key={routePath}><Link href={routePath} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-start", {
+                    'bg-nav-focus border-r-4 border-orange-1': isActive
+                  })}>
+                    <Image src={imgURL} alt={label} width={24} height={24} />
+                    <p>{label}</p>
+                  </Link></SheetClose>
+                })}
               </nav>
             </SheetClose>
+            <SignedOut>
+              <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+                <Button asChild className="text-16 w-full bg-orange-1 font-extrabold">
+                  <Link href="/sign-in">Sign in</Link>
+                </Button>
+              </div>
+            </SignedOut>
+            <SignedIn>
+              <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+                <Button className="text-16 w-full bg-orange-1 font-extrabold" onClick={() => signOut()}>
+                  <Link href="/">Log Out</Link>
+                </Button>
+              </div>
+            </SignedIn>
           </div>
         </SheetContent>
       </Sheet>
